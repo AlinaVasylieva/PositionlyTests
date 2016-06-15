3e8cfafffcf9ec743a805a966b376a05941cc0a1
AlinaVasylieva <alynamarchenko@gmail.com>
2016-06-15 18:35:03 +0200

    BaseTest class is added to be inherited, new login tests are added

---
+++ PositionlyAutomation/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace PositionlyAutomation
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+        protected string url = "https://positionly.com/";
+        protected string userEmail = "a.v.vasylieva@gmail.com";
+        protected string userPassword = "123456";
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [TestCleanup()]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+    }
+}
--- PositionlyAutomation/LoginTest.cs
+++ PositionlyAutomation/LoginTest.cs
@@ -6,39 +6,69 @@ using OpenQA.Selenium.Firefox;
 namespace PositionlyAutomation
 {
     [TestClass]
-    public class LoginTest
+    public class LoginTest : BaseTest
     {
-        IWebDriver driver;
-        string url = "https://positionly.com/";
-        string userEmail = "a.v.vasylieva@gmail.com";
-        string userPassword = "123456";
+        string _expectedTitle = "Dashboard - Positionly";
+        string _expectedPageTitle = "Login - Positionly";
+        string _expectedLoginErrorMessage = "Invalid email or password.";
 
-        string expectedTitle = "Dashboard - Positionly";
-
-        [TestInitialize()]
-        public void Initialize()
-        {
-            driver = new FirefoxDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
-            driver.Navigate().GoToUrl(url);
-        }
-
-        [TestCleanup()]
-        public void CleanUp()
-        {
-            driver.Quit();
-        }
-
-        [TestMethod()]
+        [TestMethod]
         public void SuccessfulLogin()
         {
-            driver.FindElement(By.XPath("//*[@id='navigation']/div/ul/li[3]/a")).Click();
+            driver.FindElement(By.XPath("//*[@id='header']/div/div[1]/ul/li[5]/a")).Click();
             driver.FindElement(By.XPath("//*[@id='user_email']")).SendKeys(userEmail);
             driver.FindElement(By.XPath("//*[@id='user_password']")).SendKeys(userPassword);
             driver.FindElement(By.XPath("//*[@id='new_user']/div[2]/input")).Submit();
 
-            Assert.AreEqual(expectedTitle, driver.Title);
+            Assert.AreEqual(_expectedTitle, driver.Title);
+        }
+
+        [TestMethod]
+        public void LogOut()
+        {
+            driver.FindElement(By.XPath("//*[@id='header']/div/div[1]/ul/li[5]/a")).Click();
+            driver.FindElement(By.XPath("//*[@id='user_email']")).SendKeys(userEmail);
+            driver.FindElement(By.XPath("//*[@id='user_password']")).SendKeys(userPassword);
+            driver.FindElement(By.XPath("//*[@id='new_user']/div[2]/input")).Submit();
+            driver.FindElement(By.Id("user-dropdown-trigger")).Click();
+            driver.FindElement(By.XPath("//*[@id='user-dropdown']/div[2]/ul/li[3]/a")).Click();
+
+            Assert.AreEqual(_expectedPageTitle, driver.Title);
+        }
+
+        [TestMethod()]
+        public void EmptyLoginSubmit()
+        {
+            driver.FindElement(By.XPath("//*[@id='header']/div/div[1]/ul/li[5]/a")).Click();
+            driver.FindElement(By.XPath("//*[@id='new_user']/div[2]/input")).Click();
+
+            Assert.IsTrue(driver.FindElement(By.ClassName("noty_text")).Displayed);
+            Assert.AreEqual(driver.FindElement(By.ClassName("noty_text")).Text, _expectedLoginErrorMessage);
+            Assert.AreNotEqual(_expectedTitle, driver.Title);
+        }
+
+        [TestMethod()]
+        public void LoginEmailOnly()
+        {
+            driver.FindElement(By.XPath("//*[@id='header']/div/div[1]/ul/li[5]/a")).Click();
+            driver.FindElement(By.XPath("//*[@id='user_email']")).SendKeys(userEmail);
+            driver.FindElement(By.XPath("//*[@id='new_user']/div[2]/input")).Click();
+
+            Assert.IsTrue(driver.FindElement(By.ClassName("noty_text")).Displayed);
+            Assert.AreEqual(driver.FindElement(By.ClassName("noty_text")).Text, _expectedLoginErrorMessage);
+            Assert.AreNotEqual(_expectedTitle, driver.Title);
+        }
+
+        [TestMethod()]
+        public void LoginPasswordOnly()
+        {
+            driver.FindElement(By.XPath("//*[@id='header']/div/div[1]/ul/li[5]/a")).Click();
+            driver.FindElement(By.XPath("//*[@id='user_password']")).SendKeys(userPassword);
+            driver.FindElement(By.XPath("//*[@id='new_user']/div[2]/input")).Click();
+
+            Assert.IsTrue(driver.FindElement(By.ClassName("noty_text")).Displayed);
+            Assert.AreEqual(driver.FindElement(By.ClassName("noty_text")).Text, _expectedLoginErrorMessage);
+            Assert.AreNotEqual(_expectedTitle, driver.Title);
         }
     }
 }
